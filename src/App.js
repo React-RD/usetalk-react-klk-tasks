df49b24672391c5473749c683bf9399598806e2e
Giancarlos Castillo <giancarlos@megsoftconsulting.com>
2021-02-09 20:08:07 -0400

    feat: adds source code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+
+const LOCA_STORAGE_KEY = 'todolist.app';
+
+function TodoList({ todoList, toggleTodoHandler }) {
+  return (
+      <div>
+          {todoList.map(todo => <Todo key={todo.id} todo={todo} onCheckedChange={toggleTodoHandler} /> )}
+      </div>
+  )
+}
+
+function Todo({ todo, onCheckedChange }) {
+  const onChangeCheck = () => onCheckedChange(todo.id);
+
+  return (
+      <div>
+          <input type="checkbox" checked={todo.complete} onChange={onChangeCheck} ></input>
+          {todo.title}
+      </div>
+  )
+}
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [inputValue, setInputValue] = useState('')
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  useEffect(() => {
+    const storeTodos = JSON.parse(localStorage.getItem(LOCA_STORAGE_KEY));
+    if (storeTodos) setTodos(storeTodos);
+  }, []);
+
+  
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find(todo => todo.id === id);
+
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
+  }
+  
+  useEffect(() => {
+    localStorage.setItem(LOCA_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos]);
+
+  const handleClear = () => {
+    const newTodos = todos.filter(todo => !todo.complete);
+    setTodos(newTodos);
+  } 
+  
+  const onClickHandler = () => {
+    const title = inputValue;
+    setTodos(prevTodos => { 
+      return [...prevTodos, {id: `${title}-${new Date().getTime()}`, title: title, complete: false}];
+    });
+
+    setInputValue('');
+  }
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoList todoList={todos} toggleTodoHandler={toggleTodo} />
+      <input onChange={handleInputChange} value={inputValue} type="text"></input>
+      <button onClick={onClickHandler}>Añadir tarea</button>
+      <button onClick={handleClear}>Limpiar tarea finalizadas</button>
+
+      <div>
+        { todos.filter(todo => !todo.complete).length} tareas por terminar
+      </div>
+    </>
   );
 }
 
